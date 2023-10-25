074b97cfa8bee46b4b199a97f282f22bd71b323f
Bunmi Akinola <bunconice@gmail.com>
2023-10-25 06:12:07 +0100

    updated

--- sort.h
+++ sort.h
@@ -25,7 +25,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap(int *array, int i, int j, size_t size);
-int lomuto_partition(int *array, int low, int high, size_t size);
+int partition(int *array, int low, int high, size_t size);
 void quick_sort_recursive(int *array, int low, int high, size_t size);
 
 #endif /*SORT_H*/
